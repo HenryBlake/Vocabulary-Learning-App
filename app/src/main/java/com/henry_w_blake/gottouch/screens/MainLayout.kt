37a95e538c669d6e73e0e9d11b1b1e4cef672378
Qihan Wang <97229036+HenryBlake@users.noreply.github.com>
2024-12-16 14:31:27 -0500

    Import Jetpack Compose.It will be main used in the future

---
+++ app/src/main/java/com/henry_w_blake/gottouch/screens/MainLayout.kt
@@ -0,0 +1,14 @@
+package com.henry_w_blake.gottouch.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainLayout(){
+
+}
+@Preview
+@Composable
+fun MainLayoutPreview(){
+    MainLayout()
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.henry_w_blake.gottouch" // 替換為實際包名
+    compileSdk = 34
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+}
+
+dependencies {
+    implementation(libs.activity.compose)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui.tooling.preview)
 }
